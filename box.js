7ac7a017e344f4cd5fdde9ac44284352570905a7
PravarKhandelwal <72025374+PravarKhandelwal@users.noreply.github.com>
2020-10-31 21:26:25 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,23 @@
+class box
+{
+   constructor(x,y,w,h,angle)
+   {
+    this.w = w;
+    this.h = h;
+       this.body = Bodies.rectangle(x,y,w,h)
+       
+       Matter.Body.setAngle(this.body, angle);
+       World.add(world,this.body);
+       
+   }
+   display()
+   {
+    var angle = this.body.angle;
+    push ()
+       fill ("red");
+       rotate(angle);
+       rectMode(CENTER);
+       rect(this.body.position.x,this.body.position.y,this.w,this.h); 
+       pop ()
+   }
+}
